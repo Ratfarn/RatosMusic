65579a12ea6d4886594a8da946a88807a5e1b5f0
Ratfarn <farart98@gmail.com>
2024-01-21 15:46:06 +0300

    сщььше 3

--- ProjectRatOS/MainWindow.xaml.cs
+++ ProjectRatOS/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+using Path = System.IO.Path;
 
 namespace ProjectRatOS
 {
@@ -20,18 +24,23 @@ namespace ProjectRatOS
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string curDir = Directory.GetCurrentDirectory();
+        static FileInfo fileInfo = new FileInfo(curDir);
+        static DirectoryInfo parentDir = fileInfo.Directory.Parent;
+        static string nowFolder = parentDir.FullName;
+
         public MainWindow()
         {
             InitializeComponent();
+            MusicNameCount.Minimum = 0;
+            UpdateMusic();
         }
-        
 
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true; // Отмена закрытия приложения
         }
-
         private void DragMe(object sender, MouseButtonEventArgs e)
         {
             try
@@ -42,6 +51,23 @@ namespace ProjectRatOS
             {
                 //exception
             }
+        }
+        private void LoadMusic_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Path.Combine(nowFolder, "MusicFolder"));
+            UpdateMusic();
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void UpdateMusic()//под вопросом
+        {
+            var countOfMusic = Path.Combine(nowFolder, "MusicFolder").Length;
+            MusicNameCount.Maximum = countOfMusic;
+            MusicCount.Content = $"Музыки в папке: {countOfMusic}";
         }
     }
 }
